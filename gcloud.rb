683fba43c4845daf4670e2368367488f6f3bdd7b
Holger Joest <holger@joest.org>
2016-09-19 09:42:11 +0200

    gcloud: 126.0.0

--- gcloud.rb
+++ gcloud.rb
@@ -2,14 +2,14 @@ require 'formula'
 
 class Gcloud < Formula
   homepage 'https://cloud.google.com/sdk'
-  url 'https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-124.0.0-linux-x86_64.tar.gz'
-  version '124.0.0'
-  sha256 'a36fa899fddc4d438c850c4b8484eee91fca64ee0aa6c890420124ea80861a24'
+  url 'https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-126.0.0-linux-x86_64.tar.gz'
+  version '126.0.0'
+  sha256 '3497d851b366c2ed2ce00dc36c91f8539873c30219cd5e4e7f8db64a7d732f1a'
 
   depends_on :python
 
   def install
-    system './install.sh --disable-installation-options --usage-reporting false --path-update false --bash-completion false < /dev/null'
+    system './install.sh --disable-installation-options --usage-reporting false --path-update false --bash-completion true < /dev/null'
     prefix.install Dir['bin']
     prefix.install Dir['help']
     prefix.install Dir['lib']
