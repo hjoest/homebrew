e264b25f21b176754b15a90d4d58fbff75472822
Holger Joest <holger@joest.org>
2017-01-17 16:39:24 +0100

    gcloud: 139.0.0

--- gcloud.rb
+++ gcloud.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Gcloud < Formula
   homepage 'https://cloud.google.com/sdk'
-  url 'https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-138.0.0-linux-x86_64.tar.gz'
-  version '138.0.0'
-  sha256 'dc370f43b0296c5e53ca53201b526f8de2d5fbeafc9fecdc55391bb9535fc4f8'
+  url 'https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-139.0.0-linux-x86_64.tar.gz'
+  version '139.0.0'
+  sha256 '337f7f56913fe75cb7c72dc8622f6e29544cddfca60d885682f112becf3d13fe'
 
   depends_on :python
 
