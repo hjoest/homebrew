5051b2fbfc899f719523a53ad6b887d5d805a615
Holger Joest <holger@joest.org>
2017-09-25 09:17:33 +0200

    gcloud: 172.0.1

--- gcloud.rb
+++ gcloud.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Gcloud < Formula
   homepage 'https://cloud.google.com/sdk'
-  url 'https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-172.0.0-darwin-x86_64.tar.gz'
-  version '172.0.0'
-  sha256 '22b411112d01cfffa194309c1c4aeb87ee3a95db80c02343bed12a64fe1c92cd'
+  url 'https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-172.0.1-darwin-x86_64.tar.gz'
+  version '172.0.1'
+  sha256 '31df8ec3f972d3cb5c3705118e6d468e9aee5607ebd0cf2bf2043ee6ad03c790'
 
   depends_on :python
 
